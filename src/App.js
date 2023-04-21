f493bed97091a04282e876dd4f69e809ae9e0c55
Sowjan95 <54956361+Sowjan95@users.noreply.github.com>
2023-04-20 23:19:08 -0400

    Revert "Debits"

--- src/App.js
+++ src/App.js
@@ -35,74 +35,6 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
-  async componentDidMount() {
-    // fetch data from credits API
-    const creditsResponse = await fetch("https://johnnylaicode.github.io/api/credits.json");
-    let creditsData = await creditsResponse.json();
-    creditsData.forEach(credit => {
-      this.state.creditList.push(credit);
-    });
-
-    // fetch data from debits API
-    const debitsResponse = await fetch("https://johnnylaicode.github.io/api/debits.json");
-    const debitsData = await debitsResponse.json();
-    debitsData.forEach(debit => {
-      this.state.debitList.push(debit);
-    });
-    this.updateBalance();
-  }
-
-  updateBalance() {
-    let creditTotal = 0;
-    this.state.creditList.forEach(credit => {
-      creditTotal += credit.amount;
-    });
-
-    let debitTotal = 0;
-    this.state.debitList.forEach(debit => {
-      debitTotal += debit.amount;
-    });
-
-    let newBalance = creditTotal - debitTotal;
-    this.setState({accountBalance: newBalance});
-  }
-
-  // Add a new credit to creditList
-  addCredit = (e) => {
-    e.preventDefault();
-
-    let credit = {};
-
-    credit.id = parseInt(this.state.creditList.length + 1);
-    credit.description = e.target[0].value;
-    credit.amount = parseFloat(e.target[1].value);
-    credit.date = new Date().toISOString();
-
-    this.state.creditList.push(credit);
-    e.target.reset();
-
-    this.updateBalance();
-
-  }
-
-  // Add a new credit to creditList
-  addDebit = (e) => {
-    e.preventDefault();
-
-    let debit = {};
-
-    debit.id = parseInt(this.state.debitList.length + 1);
-    debit.description = e.target[0].value;
-    debit.amount = parseFloat(e.target[1].value);
-    debit.date = new Date().toISOString();
-
-    this.state.creditList.push(debit);
-    e.target.reset();
-
-    this.updateBalance();
-
-  }
-
   // Create Routes and React elements to be rendered using React components
   render() {  
     // Create React elements and pass input props to components
@@ -111,12 +43,12 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} addCredit={this.addCredit} accountBalance={this.state.accountBalance}/>) 
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} addDebit={this.addDebit} accountBalance={this.state.accountBalance}/>) 
+    const CreditsComponent = () => (<Credits credits={this.state.creditList} />) 
+    const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/my-react-app">
+      <Router basename="/bank-of-react-example-code-gh-pages">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
